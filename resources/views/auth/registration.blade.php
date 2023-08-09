521abcd0b860e1c3bf7aa997626e2fbc910898f3
AJAYI MICHAEL <MICHAEL@MACBOOKs-MacBook-Pro.local>
2023-08-09 10:42:29 +0100

    Password hint adjusted

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -42,7 +42,7 @@
                     <div class="mt-2">
                         <input id="password" name="password" type="password" required autocomplete="password"
                                class="password block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                    <span class="custom-hint-text" style="font-size: 0.7em;">Password must include Upper case, Lower case, Number and Symbol</span>
+                    <span class="custom-hint-text" style="font-size: 0.7em;">Min:8, Upper case, Lower case, Number and Symbol</span>
                     </div>
                     @error('password')
                     <div class="text-red-500">{{ $message }}</div>
